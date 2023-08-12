738d0067406d5db882abffd2fb07768ef7b6d4f1
mikhail <mishanya-101@yandex.ru>
2023-08-12 14:55:14 +0400

    feat: pages && react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./scss/app.scss";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./components/assets/pizzas.json";
-import { useEffect, useState } from "react";
-import Skeleton from "./components/Skeleton";
+import Home from "./pages/Home";
+import NotFound from "./pages/Notfound";
+import Cart from "./pages/Cart";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://64d72bc82a017531bc13046a.mockapi.io/items")
-      .then((res) => res.json())
-      .then((data) => {
-        setItems(data);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-            {/* {items.map((obj) =>
-              isLoading ? <Skeleton /> : <PizzaBlock key={obj.id} {...obj} />
-            )} */}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <h1 style={{ textAlign: "center", padding: 100 }}>Ничего не найдено :(</h1>;
+};
+
+export default NotFound;
