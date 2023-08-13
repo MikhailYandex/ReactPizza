485da273cf55e8c70c8bed81e93c7413f78fded9
mikhail <mishanya-101@yandex.ru>
2023-08-13 15:51:41 +0400

    fix: Cart.jsx доделывание адаптива

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,10 +15,11 @@ const Home = () => {
         setItems(data);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -29,7 +30,7 @@ const Home = () => {
           ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 };
 
